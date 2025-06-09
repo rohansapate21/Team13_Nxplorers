fbcd41a294e582a9a1886b291a99102eb141d1dc
Omkark04 <omkarkangule04@gmail.com>
2025-06-09 13:24:00 +0530

    adding remaining changes

--- Resume Checker/frontend/src/App.jsx
+++ Resume Checker/frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Register from "./pages/Register";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ProtectedRoute from "./components/ProtectedRoutes";
+import './App.css'
 
 function Logout() {
     localStorage.clear();
